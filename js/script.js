92c3283abce4015b1a0b1621488c215f480350ab
Camile Marcele <mcamileparaujo@gmail.com>
2023-11-15 23:00:16 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous")
+
+
+nextButton.addEventListener('click',nextSlide)
+previousButton.addEventListener('click',previousSlide)
+let currentImage = 0;
+
+function nextSlide(){
+    if(currentImage<2){
+        currentImage++;
+        // document.getElementById("content-holder").style.transform ="translateX(-"+(currentImage*100)+"%)";
+        document.getElementById("content-holder").style.transform = `translateX(-${currentImage*100}%)`
+    }
+}
+function previousSlide(){
+    if(currentImage>0){
+        currentImage--;
+        document.getElementById("content-holder").style.transform =`translateX(-${currentImage*100}%)`
+    }
+}
