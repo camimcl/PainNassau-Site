987e8ce0d7a99e6f716c7ef63c9c0ee394fc155f
Camile Marcele <143668626+camimcl@users.noreply.github.com>
2023-11-18 01:15:28 -0300

    update

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ previousButton.addEventListener('click',previousSlide)
 let currentImage = 0;
 
 function nextSlide(){
-    if(currentImage<2){
+    if(currentImage<4){
         currentImage++;
         // document.getElementById("content-holder").style.transform ="translateX(-"+(currentImage*100)+"%)";
         document.getElementById("content-holder").style.transform = `translateX(-${currentImage*100}%)`
